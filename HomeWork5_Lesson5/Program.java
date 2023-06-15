56a0f9db0b79ca9cf21cb47803314949485e8f01
Anvar <akchalov.a.m@yandex.ru>
2023-06-15 19:19:22 +0500

    Initial commit added.

---
+++ HomeWork5_Lesson5/Program.java
@@ -0,0 +1,31 @@
+
+import models.TableModel;
+import presenters.BookingPresenter;
+import presenters.Model;
+import presenters.View;
+import views.BookingView;
+
+import java.util.Date;
+
+public class Program {
+
+    /**
+     * TODO: Домашняя работа: Метод changeReservationTable ДОЛЖЕН ЗАРАБОТАТЬ!
+     * @param args
+     */
+    public static void main(String[] args) {
+        Model tableModel = new TableModel();
+        BookingView bookingView = new BookingView();
+        BookingPresenter bookingPresenter = new BookingPresenter(tableModel, bookingView);
+        bookingPresenter.showTables();
+
+        // Клиент нажимает на кнопку "Зарезервировать", возбуждается событие,
+        // вызывается метод reservationTable()
+        bookingView.reservationTable(new Date(), 1103, "Станислав");
+
+        bookingView.reservationTable(new Date(), 102, "Станислав");
+
+        //bookingView.changeReservationTable();
+    }
+
+}
